b542bca7aafbb2b7030547013f06b4a743e65bba
Sofia Burlaka <sofiaburlaka@gmail.com>
2024-09-21 18:58:18 +0300

    added function

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,23 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K = 0.6213;
+
     public static void main(String[] args) {
-        System.out.println("Converter app.");
+        System.out.println("App for measures converting");
+        System.out.println("Version 2.0");
+
+        double km = 7;
+        double ml = 15;
+        double mile = convKmtoMl(km);
+        double kilometers = convMltoKm(mile);
+        System.out.println("Result is " + mile + " miles is " + kilometers + " kilometers");
+    }
+
+    private static double convKmtoMl(double km) {
+        return km * CONV_K;
+    }
+    private static double convMltoKm(double mile) {
+        return mile / CONV_K;
     }
 }
